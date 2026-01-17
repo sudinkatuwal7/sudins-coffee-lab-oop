cf0f4974bdb83e5b7fbda2d08d00053ee94b0dc8
Sudin Katuwal <sudcrack2024@gmail.com>
2026-01-16 23:36:19 -0600

    Initialize coffee machine simulation project
    Added a coffee machine simulation project demonstrating OOP concepts.

---
+++ main.py
@@ -0,0 +1,30 @@
+# It's version 2 of the sudins_coffee_lab 
+# ☕ A Python project demonstrating the transition from procedural programming to Object-Oriented Programming (OOP) through a coffee machine simulation.
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    choice = input(f"What would you like? { menu.get_items() }: ")
+
+    if choice == "off":
+        is_on = False
+
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+
+    else:
+        drink = menu.find_drink(choice)
+
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
